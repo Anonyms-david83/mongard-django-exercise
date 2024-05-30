f289b0de207ed8612d1fae4b2261c7e33ead554b
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-05-30 13:26:17 -0800

    tire model added and registered into admin panel

--- A/home/models.py
+++ A/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tire(models.Model) :
+    tire_name = models.CharField(max_length = 10)
+    tire_description = models.CharField(max_length = 100)
+    tire_price = models.IntegerField()
+
+    def __str__(self):
+        return self.tire_name
