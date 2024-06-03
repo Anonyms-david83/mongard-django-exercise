65f1767ac08d61412b8e78c283161727dbc0e6c2
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-06-03 05:58:49 -0800

    project closed

--- A/home/forms.py
+++ A/home/forms.py
@@ -12,7 +12,7 @@ class Tire_Update_form(forms.ModelForm):
         fields = ('tire_name' , 'tire_description' , 'tire_price')    
 
 
-class ContactUsForm(forms.Form):
+class ContactUsForm(forms.Form):                            #could be initialliezed with Forms.ModelForm
     form_contact_name = forms.CharField(max_length=20)
     form_contact_phone = forms.CharField(max_length=15)
     form_contact_body = forms.CharField(max_length=500)
